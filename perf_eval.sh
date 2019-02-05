b2c6b19c7a6698c9807c566646fcf6e97ca43f54
Pierre-Louis <pllefebvre3@gmail.com>
2019-02-05 22:06:19 +0100

    perf_eval fini

--- perf_eval.sh
+++ perf_eval.sh
@@ -4,7 +4,31 @@
 # passage a l'echelle fort
 
 # Le nombre n d'iterations qu'est lance chaque parametrage du programme
-ITERATIONS=4
+ITERATIONS=7
+
+# La mise a l'echelle maximale en nombre de coeur, pour le passage a l'echelle fort et faible
+MAX_SCALING=4
+
+# la nombre de lignes de la matrice de lettres (hauteur) de depart
+START_HEIGHT=200
+
+# la nombre de colonnes de la matrice de lettres (largeur) de depart
+START_WIDTH=100
+
+# la nombre de lignes des sous-matrices issues du decoupage du probleme (sous-hauteur) de depart
+START_UNDER_HEIGHT=20
+
+# la nombre de colonnes de la matrice de lettres (sous-largeur) de depart
+START_UNDER_WIDTH=10
+
+# le nom du fichier de resultats du passage à l'échelle fort
+STRONG_SCALING_FILE="passage_a_lechelle_fort.csv"
+
+# le nom du fichier de resultats du passage à l'échelle faible
+WEAK_SCALING_FILE="passage_a_lechelle_faible.csv"
+
+# l entete des fichiers CSV, avec le nom des colonnes
+HEADER="Nombre de coeurs,Hauteur,Largeur,Sous-Hauteur,Sous-Largeur,t1(ms),t2(ms),t3(ms),t4(ms),t5(ms),t6(ms),t7(ms),Moyenne(ms)"
 
 # Lance n fois le programme out faisant le comptage des lettres dans une matrice avec les parametres indiquees et
 # enregistre dans un CSV les resultats des temps d'execution et la moyenne
@@ -28,15 +52,65 @@ function iter_exec {
 
     printf "${CORES},${HEIGHT},${WIDTH},${UNDER_HEIGHT},${UNDER_WIDTH}," >> ${FILE}
 
-    for ((i=0 ; $ITERATIONS - $i ; i++))
+    for ((exec=0 ; $ITERATIONS - $exec ; exec++))
     do
+        echo "execution ${exec} de : ./out ${CORES} ${HEIGHT} ${WIDTH} ${UNDER_HEIGHT} ${UNDER_WIDTH}"
         time=$(./out ${CORES} ${HEIGHT} ${WIDTH} ${UNDER_HEIGHT} ${UNDER_WIDTH})
-
-        let "sum=$sum + $time"
+        time=$(echo "scale=3;$time/1000" | bc | tr -d "\n")
+        sum=$(echo "scale=3;$time + $sum" | bc | tr -d "\n")
         printf "${time}," >> ${FILE}
     done
 
-    echo "scale=3;$sum/$ITERATIONS/1000" | bc >> ${FILE}
+    echo "scale=3;$sum/$ITERATIONS" | bc >> ${FILE}
 }
 
-iter_exec 4 10 10 2 5 "test.csv"
+# Effectue le passage a l'echelle fort avec une matrice de START_HEIGHT x START_WIDTH et des sous-matrices de
+# START_UNDER_HEIGHT x START_UNDER_WIDTH
+function strong_scaling {
+
+    for (( strong_scale=1 ; ${MAX_SCALING}>=${strong_scale} ; strong_scale=${strong_scale} * 2 ))
+    do
+        echo "executions avec ${strong_scale} threads"
+        iter_exec "${strong_scale}" "${START_HEIGHT}" "${START_WIDTH}" "${START_UNDER_HEIGHT}" "${START_UNDER_WIDTH}" "${STRONG_SCALING_FILE}"
+    done
+}
+
+# Effectue le passage a l'echelle faible en commencant par une matrice de START_HEIGHT x START_WIDTH et des
+# sous-matrices de START_UNDER_HEIGHT x START_UNDER_WIDTH et en faisant doubler le nombre de threads une dimension puis
+# l'autre
+function weak_scaling {
+
+    height=${START_HEIGHT}
+    width=${START_WIDTH}
+    under_height=${START_UNDER_HEIGHT}
+    under_width=${START_UNDER_WIDTH}
+    height_growth=0
+
+    for (( weak_scale=1 ; ${MAX_SCALING}>=${weak_scale} ; weak_scale=${weak_scale} * 2 ))
+    do
+        echo "executions avec ${weak_scale} threads"
+        iter_exec "${weak_scale}" "${height}" "${width}" "${under_height}" "${under_width}" "${WEAK_SCALING_FILE}"
+
+        if [[ ${height_growth} == 1 ]]
+        then
+            (( height*=2 ))
+            (( under_height*=2 ))
+            height_growth=0
+        else
+            (( width*=2 ))
+            (( under_width*=2 ))
+            height_growth=1
+        fi
+    done
+}
+
+# script principal
+####################################################
+echo "passage a l echelle fort"
+echo "${HEADER}" > ${STRONG_SCALING_FILE}
+strong_scaling
+
+echo "passage a l echelle faible"
+echo "${HEADER}" > ${WEAK_SCALING_FILE}
+weak_scaling
+####################################################
